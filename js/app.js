d23a86baa1445f217c85e1a4a5e92cbfe785b499
gmt <gianmaria.tarsi@gmail.com>
2020-05-05 12:36:09 +0200

    Completed
    Add Scroll Effect and Responsive Mobile

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
     $('#date').countdown('2020/07/10', function(e) {
         $(this).html(e.strftime('<p><span> %D </span> days <span> %H </span> hours <span> %M </span> minutes <span> %S </span> seconds</p>'))
     });
+
+    document.querySelectorAll('.navbar .nav-link').forEach(navLink => {
+        navLink.addEventListener('click', function(e) {
+            e.preventDefault();
+           
+           document.querySelector( this.getAttribute('href') ).scrollIntoView({
+                behavior : 'smooth',
+                block : 'start'
+           })    
+        })
+    })
 });
 
 window.onscroll = function () {
