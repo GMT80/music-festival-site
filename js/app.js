aac8bbcfc78433b8a282b92a8b97cbb00d9200da
gmt <gianmaria.tarsi@gmail.com>
2020-05-05 11:03:56 +0200

    Add Nav Bar
    Add NavBar & scrolling effect

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-
     $('#date').countdown('2020/07/10', function(e) {
         $(this).html(e.strftime('<p><span> %D </span> days <span> %H </span> hours <span> %M </span> minutes <span> %S </span> seconds</p>'))
     });
-
 });
+
+window.onscroll = function () {
+    let scroll = window.scrollY;
+
+    const navigation = document.querySelector('#main-nav');
+
+    if (scroll > 300) {
+       navigation.classList.add('bg-success');
+    }
+    else {
+        navigation.classList.remove('bg-success');
+    }
+    
+}
